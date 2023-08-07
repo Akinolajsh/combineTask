157118ed9c414b7a7ee28e4d33e345c2c03f12d7
somicodes12 <somtochukwue98@gmail.com>
2023-08-07 13:30:26 +0100

    my-second-push

--- index.ts
+++ index.ts
@@ -4,3 +4,8 @@ import cors from "cors"
 const port : number = 1234
 
 const app : Application = express()
+
+const server = app.listen(port , ()=>{
+    console.log("Server is Up and and Running");
+    
+})
